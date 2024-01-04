6825aec752c15019e4ff66fd798d5f7ffe823e20
craig t smith <craig@42branches.co>
2024-01-04 17:10:51 +0100

    Add net-tools package and resolve IP address for PGHOST

--- pgagent/entrypoint.sh
+++ pgagent/entrypoint.sh
@@ -17,6 +17,17 @@ else
     exit 1
 fi
 
+# Resolve the IP address from the hostname
+PGHOST_IP=$(getent hosts $PGHOST | awk '{ print $1 }')
+
+# Check if the resolution was successful
+if [ -z "$PGHOST_IP" ]; then
+    echo "Failed to resolve the IP address for $PGHOST"
+    exit 1
+else
+    echo "Resolved $PGHOST to IP: $PGHOST_IP"
+fi
+
 # Attempt a simple connection to PostgreSQL to ensure connectivity
 echo "Attempting to connect to PostgreSQL at ${PGHOST}:${PGPORT}"
 if pg_isready -h "${PGHOST}" -p "${PGPORT}" -d "${PGDATABASE}" -U "${PGUSER}"; then
@@ -27,7 +38,7 @@ else
 fi
 
 # Construct the base connection string
-conn_str="hostaddr=${PGHOST} port=${PGPORT} dbname=${PGDATABASE} user=${PGUSER}"
+conn_str="hostaddr=${PGHOST_IP} port=${PGPORT} dbname=${PGDATABASE} user=${PGUSER}"
 
 # Check if PGPASSWORD is provided and append it to the connection string
 if [ -n "${PGPASSWORD}" ]; then
