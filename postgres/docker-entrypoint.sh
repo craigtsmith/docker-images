c3dd0cff927dc695d02b7262a49f3e88b180b042
craig t smith <craig@42branches.co>
2024-01-04 15:50:14 +0100

    Add pgAgent Dockerfile and update PostgreSQL Dockerfile
    Delete unnecessary files and configurations

--- postgres/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# docker-entrypoint.sh
-
-# Start supervisord in the background
-/usr/bin/supervisord &
-
-# Call the original entrypoint script from the official Postgres image
-exec docker-entrypoint.sh "$@"
